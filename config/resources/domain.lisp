438c01ee4f570ee59d8fcf2effa6e4ca9af4e9a6
Claire Lovisa <lovisa.claire@gmail.com>
2019-04-26 11:49:49 +0200

    Add config to domain.lisp

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,3 +1,9 @@
 (in-package :mu-cl-resources)
 
+(defparameter *cache-count-queries* nil)
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+(defparameter *max-group-sorted-properties* nil)
+
 (read-domain-file "master-log-domain.lisp")
